81a599698004a91ccac7b0d1081fc0d3c5659d14
VIVEK KUMAR <vivkumar4798@gmail.com>
2024-11-26 18:08:44 +0530

    register 500 problem solved

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { User } from "../../models/user";
 
 export async function POST(req){
+    // MongoUrl should be connected to the drivers in the mongoDb
     const body = await req.json()
     mongoose.connect(process.env.MONGO_URL);
     const createdUser = await User.create(body);
