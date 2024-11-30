1c40b00ce91952f0d29d07790238d82b43e3203a
VIVEK KUMAR <vivkumar4798@gmail.com>
2024-11-30 18:42:21 +0530

    route.js updated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,32 +1,32 @@
-import mongoose from "mongoose";
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import {User} from '../../../models/user';
-// import bcrypt from ''
+// import mongoose from "mongoose";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import {User} from '../../../models/user';
+// // import bcrypt from ''
 
-const handler = NextAuth({
-  secret: process.env.SECRET,
-    providers: [
-        CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            username: { label: "Email", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            const {email, password} = credentials;
+// const handler = NextAuth({
+//   secret: process.env.SECRET,
+//     providers: [
+//         CredentialsProvider({
+//           name: 'Credentials',
+//           credentials: {
+//             username: { label: "Email", type: "text", placeholder: "jsmith" },
+//             password: { label: "Password", type: "password" }
+//           },
+//           async authorize(credentials, req) {
+//             const {email, password} = credentials;
 
-            mongoose.connect(process.env.MONGO_URL);
-            const user = await User.findOne({email});
-            const passwordOk = user && bcrypt.CompareSync(password, user.password);            
+//             mongoose.connect(process.env.MONGO_URL);
+//             const user = await User.findOne({email});
+//             const passwordOk = user && bcrypt.CompareSync(password, user.password);            
             
-            if(passwordOk){
-              return user;
-            }
-            return null
-          }
-        })
-      ]
-});
+//             if(passwordOk){
+//               return user;
+//             }
+//             return null
+//           }
+//         })
+//       ]
+// });
 
-export { }
+// export { }
